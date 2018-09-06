b4f0f1ef3b1a738e79cba5f5bbde073a07351596
arpankbasak <35096487+arpankbasak@users.noreply.github.com>
2018-09-06 09:46:58 +0200

    Create callForBio.R

---
+++ callForBio.R
@@ -0,0 +1,33 @@
+# Loading the list of packages required for bio-computation in R
+# Prerequisite a table of bioc packages and base packages
+# Follow the example
+
+callForBio <- function(bioc_pkg = NULL, oper_pkgs = NULL, bioC = T)
+  {
+  if(is.null(bioc_pkg) != T || is.null(oper_pkgs) != T)
+  {
+    # Sourcing Bioconductor
+    source("http://www.bioconductor.org/biocLite.R")
+    bioc_pkg <- as.character(bioc_pkg)
+    
+    # Checking for biocLite call
+    if(bioC != F) biocLite(bioc_pkg, suppressUpdates = T)
+    
+    # Sourcing the operational package
+    oper_pkgs <- as.character(oper_pkgs)
+    
+    # Combining the packages for calling the library function
+    pkgs <- c(oper_pkgs, bioc_pkg)
+    
+    # Installing the operational packages
+    install_pkg <- oper_pkgs[!oper_pkgs %in% installed.packages()]
+    
+    # Calling the library function for each packages
+    for(lib in install_pkg) install.packages(lib, dependencies = T, quiet = T)
+    lapply(pkgs, library, character.only = T)
+    
+    print("Successfully loaded the above packages.")
+  }
+  else
+  {print("operational package or bioC package is an empty vector")}
+}
