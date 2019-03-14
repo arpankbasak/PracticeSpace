5d32fa713164f7762cc200eb87c223fe619dc704
arpankbasak <35096487+arpankbasak@users.noreply.github.com>
2019-03-14 16:34:23 +0100

    Colour palette %>% theme_set.R

--- theme_set_akb.R
+++ theme_set_akb.R
@@ -16,3 +16,11 @@ theme_RTN <- theme_bw() +
           plot.background=element_rect(fill="transparent", colour=NA),
           strip.background=element_rect(fill="transparent", colour=NA),
           strip.placement="outside")
+
+# Colour Palette
+col.pal <- c(c_dark_grey,  "indianred4", c_green, c_cudo_skyblue, c_dark_red, c_very_dark_green, 
+             c_red,  c_blue,  "darkseagreen4","dodgerblue1", 
+             c_dark_brown, "forestgreen", "dodgerblue4","goldenrod4", "skyblue3",
+             "deeppink3", "chocolate3", "limegreen", "orangered3", "brown3", "darkorchid3", "khaki3", "sienna3", 
+             "slateblue3", "palegreen3", "salmon3", "olivedrab3", "plum3", "thistle3", "royalblue3"
+)
